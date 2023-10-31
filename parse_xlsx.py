a932730e7ad004d1e3f6a4340b3d8ecd8f82f682
Kirill <97834479+FF3ADF61DD5F28BBCDD7F41A2313A8AB@users.noreply.github.com>
2023-11-01 00:24:47 +0500

    Update parse_xlsx.py

--- parse_xlsx.py
+++ parse_xlsx.py
@@ -1,10 +1,11 @@
 from openpyxl import load_workbook
-wb = load_workbook(filename=r'Тест файл.xlsx')
+
+wb = load_workbook(filename=r"Тест файл.xlsx")
 ws = wb.active
 m_row = ws.max_row
 m_col = ws.max_column
 for i in range(1, m_col + 1):
     for n in range(1, m_row + 1):
-        cell_obj = ws.cell(row = n, column = i)
+        cell_obj = ws.cell(row=n, column=i)
         print(cell_obj.value, end=" ")
-    print('\n')
+    print("\n")
